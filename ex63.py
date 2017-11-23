fd487ecef642b91765e55ce158f5427952038e49
BRUNO HORTA <33914960+brhorta@users.noreply.github.com>
2017-11-23 17:59:07 -0200

    Add files via upload

---
+++ ex63.py
@@ -0,0 +1,11 @@
+print('-='*20)
+print('Lembrando da sequencia de Fibonacci')
+print('-='*20)
+qts = int(input('Quantos números da sequência? '))
+cont = 0
+n_ant = 1
+n_novo = 1
+fib = n_ant + n_novo
+while cont < qts:
+    print(fib)
+    cont += 1
